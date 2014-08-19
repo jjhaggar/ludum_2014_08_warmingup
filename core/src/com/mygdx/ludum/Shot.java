4ea7938fbe7f5829a2f95365cc8582e0af328602
Victor Ramirez <virako@wadobo.com>
2014-08-19 18:47:29 +0200

    Added assets in separated class.

--- core/src/com/mygdx/ludum/Shot.java
+++ core/src/com/mygdx/ludum/Shot.java
@@ -1,28 +1,35 @@
 package com.mygdx.ludum;
 
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class Shot extends Image {
-    final Vector2 position = new Vector2();
-
     float SHOT_VELOCITY = 200f;
     float stateTime = 0;
     boolean shotGoesRight;
 
+    protected Animation animation = null;
 
-    public Shot(float x, float y, boolean facesRight){
-        this.position.x = x;
-        this.position.y = y;
+    public Shot(float x, float y, boolean facesRight, Animation animation) {
+        super(animation.getKeyFrame(0));
+        this.animation = animation;
+        this.setPosition(x, y);
         this.shotGoesRight = facesRight;
     }
 
     public void updateShot(float deltaTime){
         if (this.shotGoesRight)
-            this.position.x += deltaTime * this.SHOT_VELOCITY;
+            this.setX(this.getX() + (deltaTime * this.SHOT_VELOCITY));
         else
-            this.position.x -= deltaTime * this.SHOT_VELOCITY;
+            this.setX(this.getX() - (deltaTime * this.SHOT_VELOCITY));
 
         this.stateTime += deltaTime;
+    }
+
+    @Override
+    public void act(float delta) {
+        ((TextureRegionDrawable)this.getDrawable()).setRegion(this.animation.getKeyFrame(this.stateTime+=delta, true));
+        super.act(delta);
     }
 }
