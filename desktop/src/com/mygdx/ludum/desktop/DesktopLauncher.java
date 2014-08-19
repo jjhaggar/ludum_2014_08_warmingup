ea661d4fda4841e619469bbf6a817825d8b05cbf
Victor Ramirez <virako@wadobo.com>
2014-08-19 11:42:01 +0200

    Added setting to automatic packing.

--- desktop/src/com/mygdx/ludum/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/ludum/desktop/DesktopLauncher.java
@@ -5,7 +5,6 @@ import java.io.File;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.mygdx.ludum.LudumGame;
 
 public class DesktopLauncher {
@@ -17,9 +16,6 @@ public class DesktopLauncher {
 
 	private static void createPacker() {
 	    /* Automatic packing */
-	    Settings settings = new Settings();
-	    settings.maxWidth = 1024;
-	    settings.maxHeight = 1024;
 	    for (String folder: new String[]{"bunny", "rayaman"}) { // Added new folders here
 	        // Remove old pack
 	        for (String ext : new String[]{".png", ".pack"}) {
@@ -27,7 +23,7 @@ public class DesktopLauncher {
                 file.delete();
 	        }
 	        // Create new pack
-            TexturePacker.process(settings, "../assets/" + folder, "../android/assets/", folder + ".pack");
+            TexturePacker.process("../assets/" + folder, "../android/assets/", folder + ".pack");
 	    }
 	}
 }
